c54a95f4234a9396ca9f7be65386d283b8d20cf7
Decent-Cypher <binhngdc1911@gmail.com>
2025-06-04 17:49:16 +0000

    Added argument parsing and time logging for CUDA scripts

--- HeatEquation_FDMShared.cu
+++ HeatEquation_FDMShared.cu
@@ -1,17 +1,22 @@
+#define _POSIX_C_SOURCE 199309L
+#include <stdlib.h>
+#include <math.h>
+#include <getopt.h>
+#include <time.h>
 #include <stdio.h>
 #include <malloc.h>
 #include <cuda.h>
 #include <float.h>
-#define  N       100  /* mesh size = M = n  */
-#define  c       0.1 /* diffusion coefficient */
+int N = 100;  /* mesh size = M = n  */
+float c = 0.1; /* diffusion coefficient */
 #define  max_steps   10000  /* number of time iterations */
 /* choose Δt ≤ (Δs)^2/(2c) for stability; here a bit more conservative */
-#define  delta_s      1.0 / (N + 1)
-#define  delta_t      (delta_s * delta_s) / (4.0 * c)
+// #define  delta_s      1.0 / (N + 1)
+// #define  delta_t      (delta_s * delta_s) / (4.0 * c)
 #define BlockSizeX 4
 #define BlockSizeY 4
-#define GridSizeX  (N - 1)/BlockSizeX + 1
-#define GridSizeY  (N - 1)/BlockSizeY + 1
+// #define GridSizeX  (N - 1)/BlockSizeX + 1
+// #define GridSizeY  (N - 1)/BlockSizeY + 1
 // #define ThreadSizeX (N - 1)/(GridSizeX*BlockSizeX) + 1
 // #define ThreadSizeY (N - 1)/(GridSizeY*BlockSizeY) + 1
 
@@ -27,7 +32,7 @@ float Initial(float x, float y)
     return 0.0;
 }
 
-void InputData(float *U)
+void InputData(float *U, float delta_s)
 {
     int i, j;
     for (i=0; i<N; i++)
@@ -40,7 +45,7 @@ void InputData(float *U)
 }
 
 //=========================
-__global__ void Derivative(float *U, float *dU){
+__global__ void Derivative(float *U, float *dU, float delta_s){
     __shared__ float s_U[(BlockSizeX + 2)*(BlockSizeY + 2)];
     // T = array N x N
     // dT = array N x N
@@ -89,7 +94,7 @@ __global__ void Derivative(float *U, float *dU){
 }
 
 
-__global__ void SolvingODE(float *U,float *dU) 
+__global__ void SolvingODE(float *U,float *dU, float delta_s, float delta_t) 
 {
 
     int i, j;
@@ -101,12 +106,43 @@ __global__ void SolvingODE(float *U,float *dU)
 }
 
 
-int main() {
+int main(int argc, char *argv[]) {
+    // Parse command line arguments
+    int opt;
+    static struct option long_options[] = {
+        {"number-interior-points", required_argument, 0, 'n'},
+        {"diffusion-coefficient", required_argument, 0, 'c'},
+        {0, 0, 0, 0}
+    };
+
+    while ((opt = getopt_long(argc, argv, "n:c:", long_options, NULL)) != -1) {
+        switch (opt) {
+            case 'n':
+                N = atoi(optarg);
+                break;
+            case 'c':
+                c = atof(optarg);
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [--number-interior-points N] [--diffusion-coefficient c]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    double delta_s = 1.0 / (N + 1);
+    double delta_t = 0.5 * (delta_s * delta_s) / (4.0 * c);
+    int GridSizeX = (N - 1) / BlockSizeX + 1;
+    int GridSizeY = (N - 1) / BlockSizeY + 1;
+
+    /* Start timing */
+    struct timespec start_time, end_time;
+    clock_gettime(CLOCK_MONOTONIC, &start_time);
+
     int i, j, t;
     float *Ucpu, *dUcpu;
     Ucpu  = (float *)malloc((N * N)*sizeof(float));
     dUcpu = (float *)malloc((N * N)*sizeof(float));
-    InputData(Ucpu);
+    InputData(Ucpu, delta_s);
     // CUDA code
     //1. Delare and Allocate Mem on GPU
     float *Ugpu,*dUgpu;
@@ -121,8 +157,7 @@ int main() {
     dim3 dimBlock(BlockSizeX,BlockSizeY);
 
     for (t=0; t<max_steps; t++) {
-        Derivative<<<dimGrid,dimBlock>>>(Ugpu,dUgpu);
-        SolvingODE<<<dimGrid,dimBlock>>>(Ugpu,dUgpu);
+        Derivative<<<dimGrid,dimBlock>>>(Ugpu,dUgpu, delta_s);      SolvingODE<<<dimGrid,dimBlock>>>(Ugpu,dUgpu, delta_s, delta_t);
         cudaDeviceSynchronize();
     }
 
@@ -130,6 +165,18 @@ int main() {
     cudaMemcpy(Ucpu, Ugpu, (N * N)*sizeof(float),cudaMemcpyDeviceToHost);
     for (i = 0; i < N; i++ )
         for (j = 0; j < N; j++) printf("%f \n",*(Ucpu+i*N+j));
+
+    /* End timing */
+    clock_gettime(CLOCK_MONOTONIC, &end_time);
+    double elapsed_time = (end_time.tv_sec - start_time.tv_sec) +
+                        (end_time.tv_nsec - start_time.tv_nsec) / 1e9;
+    /* Display configuration and timing */
+    printf("Simulation completed.\n");
+    printf("Number of interior points (N): %d\n", N);
+    printf("Diffusion coefficient (c): %.6f\n", c);
+    printf("Total execution time: %.6f seconds\n", elapsed_time);
+    printf("Number of iterations: %d\n", max_steps);
+    
     //6. Free Mem on CPU and GPU
     free(Ucpu); free(dUcpu);
     cudaFree(Ugpu);cudaFree(dUgpu);
